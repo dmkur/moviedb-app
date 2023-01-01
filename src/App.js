69641266664ff3b83301f7b970f115f812c03b25
dmkur <fgameua@gmail.com>
2023-01-01 21:04:44 +0200

    add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import {useSelector} from "react-redux";
-
 const App = () => {
-    const {movie} = useSelector(state => state.movieReducer);
-    console.log(movie)
+
     return (
         <div>
-
             App component
-
         </div>
     )
 };
