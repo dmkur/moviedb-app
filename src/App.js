d72a6ded47f6e62286453453f78fdd7de8c3cacd
dmkur <fgameua@gmail.com>
2023-01-08 20:13:48 +0200

    add basis routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import {MainLayout} from "./layouts/MainLayout";
+import {MainLayout} from "./layouts";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {GenresPage, MoviesPage} from "./pages";
 
 const App = () => {
-  return (
-      <div>
-        <MainLayout/>
-      </div>
-  )
+    return (
+        <Routes>
+            <Route path={''} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'/movies'}/>}/>
+                <Route path={'/movies'} element={<MoviesPage/>}/>
+                <Route path={'/genres'} element={<GenresPage/>}/>
+            </Route>
+        </Routes>
+    )
 };
 
 export {App};
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,9 +1,14 @@
+import {Outlet} from "react-router-dom";
+import {Header} from "../components";
+
 const MainLayout = () => {
-  return (
-      <div>
-        MainLayout component
-      </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <hr/>
+            <Outlet/>
+        </div>
+    )
 };
 
 export {MainLayout};
